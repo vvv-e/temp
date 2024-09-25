2bb03fbfcf7ae36ce9d98f1eeef8f88e76ba2d15
vvv-e <vvedensky@subi.su>
2024-09-25 12:52:23 +0500

    Add files via upload

---
+++ sd_base.py
@@ -0,0 +1,15 @@
+# Базовые структуры данных
+
+# 1st program
+print( 9**0.5 * 5)
+
+# 2nd program
+print( 9.99 > 9.89 and 1000 != 1000.1 )
+
+# 3rd program
+print( 2 * 2 + 2 )
+print( 2 * ( 2 + 2 ) )
+print( 2 * 2 + 2 == 2 * ( 2 + 2 ) )
+
+# 4th program
+print( int( 123.456 * 10. ) % 10 )
