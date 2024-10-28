19d928a962357cb24a2e93cb24eb3154c8d81ccf
vvv-e <vvedensky@subi.su>
2024-10-28 14:26:59 +0500

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,18 +1,1 @@
-class MultiAttr:
-    def __init__(self):
-        self.about = "Тестовый экземпляр для множественной инициации атрибутов в цикле"
-
-    def set_attribute(self, turn):
-        for num in range(turn):
-            setattr(self, f"variable_{num}", num ** 2)
-
-    def get_attribute(self, turn):
-        for num in range(turn):
-            print(getattr(self, f"variable_{num}"), end=" ")
-
-if __name__ == '__main__':
-    turn = 10
-    a = MultiAttr()
-    a.set_attribute(turn)
-    print(dir(a)[-turn - 3:])
-    a.get_attribute(turn)
+pass
