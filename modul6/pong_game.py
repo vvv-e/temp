63e9af97401bc5d3e3b71aff4bb1f562fa0cd8d1
vvv-e <vvedensky@subi.su>
2024-11-11 13:15:13 +0500

    \0/

--- modul6/pong_game.py
+++ modul6/pong_game.py
@@ -2,7 +2,7 @@ import arcade
 
 class Game(arcade.Window):
     def on_draw(self):
-        self.clear((255,255,0))
+        self.clear((127,255,127))
 
 if __name__ == '__main__':
     windows = Game()
