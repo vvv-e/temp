dbc52e3af9e7a6736733ca91525678fb4d01ebf3
vvv-e <vvedensky@subi.su>
2024-11-12 01:34:17 +0500

    edit pong_game

--- modul6/pong_game.py
+++ modul6/pong_game.py
@@ -1,9 +1,47 @@
 import arcade
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Pong game"
+
+
+class Ball(arcade.Sprite):
+    def __init__(self):
+        super().__init__('ball.png', 0.15)
+        self.change_x = 0.5
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+
+class Bar(arcade.Sprite):
+    def __init__(self):
+        super().__init__('bar.png', 0.5)
+
+
 class Game(arcade.Window):
+    def __init__(self, width, hight, title):
+        super().__init__(width, hight, title)
+        self.bar = Bar()
+        self.ball = Ball()
+        self.setup()
+
+    def update(self, delta_time: float):
+        self.ball.update()
+
     def on_draw(self):
-        self.clear((127,255,127))
+        self.clear((127, 255, 255))
+        self.bar.draw()
+        self.ball.draw()
+
+    def setup(self):
+        self.bar.center_x = SCREEN_WIDTH / 2.
+        self.bar.center_y = SCREEN_HEIGHT / 8.
+        self.ball.center_x = SCREEN_WIDTH / 2.
+        self.ball.center_y = SCREEN_HEIGHT / 2.
+
 
 if __name__ == '__main__':
-    windows = Game()
+    windows = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     arcade.run()
